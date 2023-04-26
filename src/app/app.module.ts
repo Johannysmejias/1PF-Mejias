6ca6961bb9567a76aa8e3273c409a4523b8abb90
Johannys <mejiasjohannys@gmail.com>
2023-04-26 17:39:09 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormulariosComponent } from './components/formularios/formularios.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    DashboardComponent,
+    FormulariosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/formularios/formularios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formularios',
+  templateUrl: './formularios.component.html',
+  styleUrls: ['./formularios.component.scss']
+})
+export class FormulariosComponent {
+
+}
